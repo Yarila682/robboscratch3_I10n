f392340e0067ab2180514a9c646936940b7a59c1
Yarila <yarila682@yandex.ru>
2018-09-03 20:37:42 +0300

    paint-editor russian localization

--- locales/paint-msgs.js
+++ locales/paint-msgs.js
@@ -24,24 +24,24 @@ const paintMsgs = {
   },
   "ru": {
     "messages": {
-      "paint.paintEditor.redo": "Redo",
-      "paint.paintEditor.fill": "Fill",
-      "paint.modeTools.eraserSize": "Eraser size",
-      "paint.paintEditor.backward": "Backward",
-      "paint.paintEditor.group": "Group",
-      "paint.modeTools.paste": "Paste",
-      "paint.paintEditor.saturation": "Saturation",
-      "paint.modeTools.brushSize": "Brush size",
-      "paint.paintEditor.undo": "Undo",
-      "paint.paintEditor.forward": "Forward",
-      "paint.paintEditor.brightness": "Brightness",
-      "paint.paintEditor.costume": "Costume",
-      "paint.paintEditor.back": "Back",
-      "paint.paintEditor.ungroup": "Ungroup",
-      "paint.paintEditor.hue": "Color",
-      "paint.modeTools.copy": "Copy",
-      "paint.paintEditor.front": "Front",
-      "paint.paintEditor.stroke": "Outline"
+      "paint.paintEditor.redo": "Повторить",
+      "paint.paintEditor.fill": "Заливка",
+      "paint.modeTools.eraserSize": "Ширина ластика",
+      "paint.paintEditor.backward": "Назад",
+      "paint.paintEditor.group": "Группировать",
+      "paint.modeTools.paste": "Вставить",
+      "paint.paintEditor.saturation": "Насыщенность",
+      "paint.modeTools.brushSize": "Размер",
+      "paint.paintEditor.undo": "Отменить",
+      "paint.paintEditor.forward": "Вперёд",
+      "paint.paintEditor.brightness": "Яркость",
+      "paint.paintEditor.costume": "Костюм",
+      "paint.paintEditor.back": "Назад",
+      "paint.paintEditor.ungroup": "Разгруппировать",
+      "paint.paintEditor.hue": "Цвет",
+      "paint.modeTools.copy": "Копировать",
+      "paint.paintEditor.front": "На передний план",
+      "paint.paintEditor.stroke": "Контур"
     }
   }
 }
